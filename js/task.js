c013c6bedd2da363c3918aeaf1346397eb36e002
mathieu Dev <mathieu.gillet@hotmail.fr>
2021-11-16 22:45:47 +0100

    create flies js

---
+++ js/task.js
@@ -0,0 +1,9 @@
+const task = {
+
+taskEvent : function () {
+//Pour masquer la div Profil
+const profil = document.querySelector(hide);
+profil.style.display = none;
+//Pour afficher la div Profil
+    }    
+}
