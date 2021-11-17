a65f0c21b5c731108cc215c89633852d7fc64525
mathieu Dev <mathieu.gillet@hotmail.fr>
2021-11-17 21:04:47 +0100

    add feature js profil hide

--- js/task.js
+++ js/task.js
@@ -2,8 +2,8 @@ const task = {
 
 taskEvent : function () {
 //Pour masquer la div Profil
-const profil = document.querySelector(hide);
-profil.style.display = none;
+const profil = document.querySelector('.hide');
+profil.style.display = 'none';
 //Pour afficher la div Profil
     }    
 }
