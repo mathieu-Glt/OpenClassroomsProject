056744b1c336814c09a44b234de1347ba26cffab
mathieu Dev <mathieu.gillet@hotmail.fr>
2021-11-23 14:04:53 +0100

    corection paragraph presentation

--- js/task.js
+++ js/task.js
@@ -3,6 +3,7 @@ const task = {
 taskEvent : function () {
 //Pour masquer la div Profil
 const profil = document.querySelector('.hide');
+//console.log(profil);
 profil.style.display = 'none';
 //Pour afficher la div Profil
 let togg1 = document.getElementById("togg1");
